944cc7a122eff4bc9b2707d93cec1b6b956234e2
socastro7 <socastro@itba.edu.ar>
2022-05-19 17:04:46 -0300

    proyecto mercado liebre

---
+++ app.js
@@ -0,0 +1,23 @@
+//tecnica mobile first --> mas lindo, mejor estructurado
+const PORT = 3000;
+
+const express = require("express");
+const path = require("path")
+const app = express();
+
+//dejo accesible la carpeta public
+//uso public como si ya estaria en esa carpeta
+app.use(express.static(path.join(__dirname, "public")));
+
+//accedo con localhost:3000
+app.listen(PORT, ()=>{console.log("Servidor corriendo en el puerto", PORT)});
+
+//parametros: 1--> path, 2--> handler, funcion controladora
+app.get("/", (req,res)=>{   
+    res.sendFile(path.join(__dirname, "./views/home.html")) });
+
+app.get("/register", (req,res)=>{   
+    res.sendFile(path.join(__dirname, "./views/register.html")) });
+
+app.get("/login", (req,res)=>{   
+    res.sendFile(path.join(__dirname, "./views/login.html")) });
